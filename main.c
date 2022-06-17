5f4863a9b75d754acfe0992af5185a17477ae5da
Eyad <86027275+Eyad-1@users.noreply.github.com>
2022-06-17 19:19:10 +0200

    Avr code uploaded
    ADC

--- main.c
+++ main.c
@@ -1,27 +1,25 @@
 /*
- * Assignment 1 (toggle 2 LEDs with 2 switches).c
+ * ADC 1.c
  *
- * Created: 3/8/2022 12:11:50 AM
+ * Created: 3/29/2022 1:42:07 PM
  * Author : Eyad
  */ 
-#ifndef F_CPU
-#define F_CPU 16000000UL // 16 MHz clock speed
-#endif
+
 #include <avr/io.h>
-#include <util/delay.h>
-
-
-int main(void)
+#define F_CPU 16000000UL
+int main (void)
 {
-    DDRB = 0b11111100;
-    while (1) 
-    {
-		if((PINB & (0b00000001)) == 1){
-			PORTB = 0b00000100; // turn yellow LED ON, turn red LED OFF
-		}
-		else if((PINB & (0b00000010)) == 2){
-			PORTB = 0b00001000; // turn yellow LED OFF, turn red LED ON
-		}
-    }
+	DDRB = 0xFF;//make Port B an output
+	DDRD = 0xFF; //make Port D an output
+	ADCSRA= 0x87;//make ADC enable and select ck/128
+	ADMUX= 0x40;
+	while(1)
+	{
+		ADCSRA |= (1<<ADSC);//start conversion
+		while((ADCSRA&(1<<ADIF))==0);//wait for conversion to finish
+		ADCSRA |= (1<<ADIF);
+		PORTD = ADCL;//give the low byte to PORTD
+		PORTB = ADCH;//give the high byte to PORTB
+	}
 }
 
