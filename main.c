f8aa7ca6b2a03b55bb5e233ae382e5de61a2c996
Eyad <86027275+Eyad-1@users.noreply.github.com>
2022-06-17 19:20:38 +0200

    Avr code uploaded
    Interrupt led

--- main.c
+++ main.c
@@ -1,25 +1,16 @@
-/*
- * ADC 1.c
- *
- * Created: 3/29/2022 1:42:07 PM
- * Author : Eyad
- */ 
-
 #include <avr/io.h>
-#define F_CPU 16000000UL
-int main (void)
+#include <avr/interrupt.h>
+int main(void)
 {
-	DDRB = 0xFF;//make Port B an output
-	DDRD = 0xFF; //make Port D an output
-	ADCSRA= 0x87;//make ADC enable and select ck/128
-	ADMUX= 0x40;
-	while(1)
-	{
-		ADCSRA |= (1<<ADSC);//start conversion
-		while((ADCSRA&(1<<ADIF))==0);//wait for conversion to finish
-		ADCSRA |= (1<<ADIF);
-		PORTD = ADCL;//give the low byte to PORTD
-		PORTB = ADCH;//give the high byte to PORTB
-	}
+      DDRB=1<<5;
+	  PORTD=1<<2;
+	  EICRA=0X2;
+	  EIMSK=(1<<INT0);
+	  sei();
+	  while(1);
+	  
 }
-
+ISR (INT0_vect)
+{
+	PORTB^=(1<<5);
+}
